8f10da75de932ce335e2cb5cd244678a33832408
Victor Navascues <wiviktor@gmail.com>
2020-11-16 20:50:06 +0000

    Added GET events history (new endpoint)
     - Added new endpoint (moved out from GET trades history)
    
     - Added DB add & get methods in `dbhandler.py`
    
     - [FIX] Trades and events endpoints do not share `reset_db_data`
     logic. Current function splitted in two DB deletion methods.
    
     - Includes first feature feedback, e.g. renaming, lp_amount,
     archive node TODO, group events by LP, etc.
    
     - Pending tests

--- rotkehlchen/chain/ethereum/uniswap/graph.py
+++ rotkehlchen/chain/ethereum/uniswap/graph.py
@@ -102,3 +102,88 @@ SWAPS_QUERY = (
     }}}}
     """
 )
+
+# Get LP events queries
+# ! TODO: feeTo, feeLiquidity?
+MINTS_QUERY = (
+    """
+    mints
+    (
+        first: $limit,
+        skip: $offset,
+        where: {{
+            to: $address,
+            timestamp_gte: $start_ts,
+            timestamp_lte: $end_ts,
+        }}
+    ) {{
+        transaction {{
+            id
+        }}
+        logIndex
+        timestamp
+        to
+        pair {{
+            id
+            token0 {{
+                id
+                decimals
+                name
+                symbol
+            }}
+            token1 {{
+                id
+                decimals
+                name
+                symbol
+            }}
+        }}
+        amount0
+        amount1
+        amountUSD
+        liquidity
+    }}}}
+    """
+)
+
+# ! TODO VN: needsComplete, feeTo, feeLiquidity?
+BURNS_QUERY = (
+    """
+    burns
+    (
+        first: $limit,
+        skip: $offset,
+        where: {{
+            sender: $address,
+            timestamp_gte: $start_ts,
+            timestamp_lte: $end_ts,
+        }}
+    ) {{
+        transaction {{
+            id
+        }}
+        logIndex
+        timestamp
+        sender
+        pair {{
+            id
+            token0 {{
+                id
+                decimals
+                name
+                symbol
+            }}
+            token1 {{
+                id
+                decimals
+                name
+                symbol
+            }}
+        }}
+        amount0
+        amount1
+        amountUSD
+        liquidity
+    }}}}
+    """
+)
